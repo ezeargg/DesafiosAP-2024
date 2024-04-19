27db637221f438ed0dd574dfd174b93a4c0d553d
Ezequiel Segovia <ezesegovia2016@gmail.com>
2024-04-19 20:06:34 -0300

    prueba

---
+++ Desafio1/main.js
@@ -0,0 +1,9 @@
+let Nombre = prompt("Ingresa tu nombre");
+let edad= prompt("Agrega tu edad: ");
+let anioAct= prompt("Ingrese los cuatros digitos del año actual");
+let nacimiento= parseInt(anioAct - edad);
+
+
+
+
+console.log("Tu nombre es: " + Nombre);
