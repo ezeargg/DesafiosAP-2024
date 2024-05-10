71994bd7771c92ecb5458b37700b69e30f350ccf
Ezequiel Segovia <ezesegovia2016@gmail.com>
2024-05-10 18:43:13 -0300

    Agregue desafio 4

---
+++ Desafio4/assets/main.js
@@ -0,0 +1,8 @@
+function sumarProductos (precioUnitario, cantidadDeseada){
+    return totalGastado = precioUnitario * cantidadDeseada;
+}
+
+let precio = 5500;
+let cantidad = parseInt(prompt(`Ingrese la cantidad que quiera comprar: `));
+sumarProductos(precio, cantidad);
+alert(sumarProductos(precio, cantidad))
