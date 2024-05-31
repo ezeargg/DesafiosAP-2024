8962b5aa2b303c3a522fc38f19766fbb7866b32b
Ezequiel Segovia <ezesegovia2016@gmail.com>
2024-05-31 20:15:06 -0300

    agregue desafio 6

---
+++ Desafio6/assets/main.js
@@ -0,0 +1,9 @@
+let productos = {
+    nombre: "Camiseta",
+    precio: 7500,
+    cantidadDisponible: 50
+}
+
+productos.nombre = "Zapatillas"
+productos.categoria= "Calzado"
+delete productos.stock
