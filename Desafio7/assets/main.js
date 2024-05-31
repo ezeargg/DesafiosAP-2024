bf9881bf39760be5c7fda18b239575038d296ecd
Ezequiel Segovia <ezesegovia2016@gmail.com>
2024-05-31 20:48:14 -0300

    desafio 7 agregado

--- Desafio7/assets/main.js
+++ Desafio7/assets/main.js
@@ -1,1 +1,8 @@
-let preciop = document.getElementById()
+let precioPro = document.getElementById("precioProducto");
+
+setTimeout(() => {
+    precioPro.innerText = "Precio: $450";
+    let titulo = document.querySelector("h2");
+    titulo.style.color = "red";
+}, 4000);
+
